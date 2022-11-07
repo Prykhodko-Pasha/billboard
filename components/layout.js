924c0c9a4577518fcd54f287bb9bf30366fb661d
Prykhodko-Pasha <prykhodko.p@gmail.com>
2022-11-08 01:34:28 +0200

    turn back

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import AppBar from "./appBar";
+import AppBar from "../components/appBar";
 import styles from "../styles/Home.module.css";
 
 export default function Layout({ children }) {
